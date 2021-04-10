58475427bb703cfbe1d21f2dc62d263eeb611b46
berkanil.kizmaz <berkanil.kizmaz@cfn.com.tr>
2021-04-11 01:28:38 +0300

    20. Ödev

--- Entities/DTOs/RentalDetailDto.cs
+++ Entities/DTOs/RentalDetailDto.cs
@@ -7,14 +7,6 @@ namespace Entities.DTOs
 {
     public class RentalDetailDto : IDto
     {
-        //    public int RentalId { get; set; }
-        //    public string CarName { get; set; }
-        //    public string BrandName { get; set; }
-        //    public string UserName { get; set; }
-        //    public string CompanyName { get; set; }
-        //    public DateTime RentDate { get; set; }
-        //    public DateTime? ReturnDate { get; set; }
-
         public int RentalId { get; set; }
 
         public DateTime RentDate { get; set; }
